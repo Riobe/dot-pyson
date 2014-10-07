22bd313f717310267dab2d0b7bfc3dd91792590f
riobe <jeremy.pridemore@gmail.com>
2014-10-06 19:17:43 -0600

    Fixing view bug by referencing module variable.

--- config.py
+++ config.py
@@ -3,22 +3,17 @@
 import json
 from pprint import pprint
 
-json_path = None
-json_data = None
+__json_path = None
+__json_data = None
 
 def load(file_path):
-    global json_data
-    global json_path
+    global __json_data
+    global __json_path
 
-    json_path = file_path
-    with open(json_path) as json_file:
-        json_data = json.load(json_file)
-
-def debug():
-    for line in json_file.readlines():
-        print(line)
-    load(json_path)
+    __json_path = file_path
+    with open(__json_path) as json_file:
+        __json_data = json.load(json_file)
 
 def view():
-    print(json.dumps(json_data, sort_keys=True, indent=4, separators=(',', ': ')))
+    print(json.dumps(__json_data, sort_keys=True, indent=4, separators=(',', ': ')))
 
