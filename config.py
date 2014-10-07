1d4811e83ae41dd942aae9231ff859eca62a3b27
riobe <jeremy.pridemore@gmail.com>
2014-10-07 17:01:20 -0600

    Make sorted file always display sorted.

--- config.py
+++ config.py
@@ -5,14 +5,18 @@ from collections import OrderedDict
 
 __json_path = None
 __json_data = None
+__sorted = None
 
 def load(file_path, load_sorted):
     global __json_data
     global __json_path
+    global __sorted
 
+    __sorted = load_sorted
     __json_path = file_path
+
     with open(__json_path) as json_file:
-        if load_sorted:
+        if __sorted:
             unsorted_data = json.load(json_file)
             __json_data = json.loads(json.dumps(unsorted_data, sort_keys=True), object_pairs_hook = OrderedDict)
         else:
@@ -20,7 +24,7 @@ def load(file_path, load_sorted):
             
 def to_string(path=None):
     data = __json_data[path] if path else __json_data
-    return json.dumps(data, indent=4, separators=(',', ': '))
+    return json.dumps(data, sort_keys=__sorted, indent=4, separators=(',', ': '))
 
 def keys_at(path):
     data = __json_data[path] if path else __json_data
